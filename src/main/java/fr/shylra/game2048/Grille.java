a53e8a99018800baaf48626b537cac4bfedf251a
Shylra <sorashirohikikomori@gmail.com>
2021-08-11 10:56:48 +0200

    déplacement haut

--- src/main/java/fr/shylra/game2048/Grille.java
+++ src/main/java/fr/shylra/game2048/Grille.java
@@ -87,9 +87,41 @@ public class Grille {
 	}
 	
 	public void gauche() {
+		
 	}
 	public void droite() {
 	}
+	
 	public void haut() {
+		int l;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				if (this.map[i][j] != 0 && i < 3) {
+					for (int k = i+1; k < 4; k++) {
+						if (this.map[k][j] != 0) {
+							if (this.map[i][j] == this.map[k][j]) {
+								this.map[i][j] *= 2;
+								this.map[k][j] = 0;
+							}
+							k = 4;
+						}
+					}
+				}
+			}
+		}
+		for (int i = 0; i < 4; i++) {
+			l = i;
+			for (int j = 0; j < 4; j++) {
+				if (this.map[i][j] != 0) {
+					for (int k = i-1; k >= 0; k--) {
+						if (this.map[k][j] == 0) {
+							this.map[k][j] = this.map[l][j];
+							this.map[l][j] = 0;
+							l = k;
+						}
+					}
+				}
+			}
+		}
 	}
 }
