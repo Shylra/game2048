7decf11b9efc2fc3ae16c436de6a4e1d682d8ca4
Shylra <sorashirohikikomori@gmail.com>
2021-08-11 10:47:57 +0200

    correction de l'affichage

--- src/main/java/fr/shylra/game2048/Affiche.java
+++ src/main/java/fr/shylra/game2048/Affiche.java
@@ -6,8 +6,19 @@ public class Affiche {
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 4; j++) {
 				System.out.print(grille.map[i][j]);
+				if (grille.map[i][j] < 10) {
+					System.out.print("     ");
+				} else if (grille.map[i][j] < 100) {
+					System.out.print("    ");
+				} else if (grille.map[i][j] < 1000) {
+					System.out.print("   ");
+				} else if (grille.map[i][j] < 10000) {
+					System.out.print("  ");
+				} else {
+					System.out.print(" ");
+				}
 			}
-			System.out.println();
+			System.out.println("\n");
 		}
 		System.out.println();
 	}
